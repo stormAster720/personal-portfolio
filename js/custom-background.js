385ba586fdf619c4e7e064d41c4ef38eb8f75f65
Luis Felipe Velasquez Garcia <pipevelasquez7@gmail.com>
2023-06-04 21:37:41 -0500

    added custom java for gradients on each hero image

---
+++ js/custom-background.js
@@ -0,0 +1,10 @@
+const customBackground = document.querySelectorAll('.introductory__hero-container-image');
+
+// Iterate over the selected elements
+customBackground.forEach((img) => {
+  // Get the value of the "data-url" attribute
+  const imageUrl = img.dataset.url;
+
+  // Set the background image URL for each element as well as their gradient
+  img.style.backgroundImage = ` linear-gradient(rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5)50%), url(${imageUrl})`;
+});
